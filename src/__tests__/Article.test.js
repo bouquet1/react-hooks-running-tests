d323cee0e48b5402873804c75a10e7b91d7fe4fc
bouquet1 <bukuleta@gmail.com>
2023-05-18 23:28:19 -0700

    passes the tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,9 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  //screen.debug();
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
+  //expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
